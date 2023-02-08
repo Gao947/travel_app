4bbc0bd56564812080423a949c4dc6f926a87a61
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-02-08 15:45:08 +1100

    Route

--- react-travel/src/App.tsx
+++ react-travel/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import styles from "./App.module.css";
-import { BrowserRouter, Route } from "react-router-dom";
-import { HomePage } from './pages';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages";
 
 function App() {
   return (
     <div className={styles.App}>
       <BrowserRouter>
-        <Route path="/" component={HomePage} />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signIn" element={ <h1>登录页面</h1>} />
+          <Route path="" element={ <h1> 404 not found ！</h1>} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
