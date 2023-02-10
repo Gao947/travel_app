300f96a025e51f3f9d9f6d82c297aebc68b8b791
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-02-10 13:47:08 +1100

    fix 404 route

--- react-travel/src/App.tsx
+++ react-travel/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/signIn" element={ <h1>登录页面</h1>} />
-          <Route path="" element={ <h1> 404 not found ！</h1>} />
+          <Route path="*" element={ <h1> 404 not found !</h1>} />
         </Routes>
       </BrowserRouter>
     </div>
