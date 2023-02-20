ea8d1d55fe6a70115e0c5f317cfbdccd9c586e7f
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-02-20 18:50:45 +1100

    user token

--- react-travel/src/pages/signIn/SignInForm.tsx
+++ react-travel/src/pages/signIn/SignInForm.tsx
@@ -1,51 +1,81 @@
 import styles from "./SignInForm.module.css";
-import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Form, Input, Button, Checkbox } from "antd";
+import { signIn } from "../../redux/user/slice";
+import { useDispatch } from "react-redux";
+import { useSelector } from "../../redux/hooks";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import React from "react";
 
-const onFinish = (values: any) => {
-  console.log('Success:', values);
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
 };
 
-const onFinishFailed = (errorInfo: any) => {
-  console.log('Failed:', errorInfo);
+export const SignInForm = () => {
+
+  const loading = useSelector(s => s.user.loading)
+  const jwt = useSelector(s => s.user.token)
+  const error = useSelector(s => s.user.error)
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(()=>{ 
+    if(jwt !== null) {
+      navigate("/");
+    }
+  }, [jwt])
+
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+    dispatch(signIn({
+      email: values.username,
+      password: values.password
+    }))
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      className={styles["register-form"]}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit" loading={loading}>
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
-
-export const SignInForm: React.FC = () => (
-  <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-    className={styles["register-form"]}
-  >
-    <Form.Item
-      label="Username"
-      name="username"
-      rules={[{ required: true, message: 'Please input your username!' }]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item
-      label="Password"
-      name="password"
-      rules={[{ required: true, message: 'Please input your password!' }]}
-    >
-      <Input.Password />
-    </Form.Item>
-
-    <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-      <Checkbox>Remember me</Checkbox>
-    </Form.Item>
-
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
--- react-travel/src/redux/store.ts
+++ react-travel/src/redux/store.ts
@@ -7,12 +7,14 @@ import { productDetailSlice } from "./productDetail/slice";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { getDefaultMiddleware } from "@reduxjs/toolkit/dist/getDefaultMiddleware";
 import { productSearchSlice } from "./productSearch/slice"; 
+import { userSlice } from "./user/slice";
 
 const rootReducer = combineReducers({
     language: languageReducer,
     recommendProducts: recommendProductsReducer,
     productDetail: productDetailSlice.reducer,
-    productSearch: productSearchSlice.reducer
+    productSearch: productSearchSlice.reducer,
+    user: userSlice.reducer
 })
 
 // const store = createStore(rootReducer, applyMiddleware(thunk, actionLog));
