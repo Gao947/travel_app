bc6096e48528d04510483cae4db56438c47cde1a
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-02-20 15:56:38 +1100

    RegisterForm 2

--- react-travel/src/pages/register/RegisterForm.tsx
+++ react-travel/src/pages/register/RegisterForm.tsx
@@ -1,47 +1,67 @@
-import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
-import styles from "../register/RegisterForm.module.css";
+import { Form, Input, Button, Checkbox } from "antd";
+import styles from "./RegisterForm.module.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import React from "react";
 
-const onFinish = (values: any) => {
-  console.log('Success:', values);
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
 };
 
-const onFinishFailed = (errorInfo: any) => {
-  console.log('Failed:', errorInfo);
-};
+export const RegisterForm = () => {
+  const navigate = useNavigate();
 
-export const RegisterForm: React.FC = () => (
-  <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-    className={styles['register-form']}
-  >
-    <Form.Item
-      label="Username"
-      name="username"
-      rules={[{ required: true, message: 'Please input your username!' }]}
+  const onFinish = async (values: any) => {
+    console.log("Success:", values);
+    try {
+      await axios.post("http://123.56.149.216:8080/auth/register", {
+        email: values.username,
+        password: values.password,
+        confirmPassword: values.confirm,
+      });
+      navigate("/signIn/");
+    } catch (error) {
+      alert("注册失败！");
+    }
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      className={styles["register-form"]}
     >
-      <Input />
-    </Form.Item>
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
 
-    <Form.Item
-      label="Password"
-      name="password"
-      rules={[{ required: true, message: 'Please input your password!' }]}
-    >
-      <Input.Password />
-    </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password />
+      </Form.Item>
 
-    <Form.Item
+      <Form.Item
         label="Confirm Password"
         name="confirm"
-        hasFeedback //验证成功会打钩
+        hasFeedback
         rules={[
           { required: true, message: "Please input your confirm password!" },
           ({ getFieldValue }) => ({
@@ -53,18 +73,19 @@ export const RegisterForm: React.FC = () => (
             },
           }),
         ]}
-    >
-      <Input.Password />
-    </Form.Item>
+      >
+        <Input.Password />
+      </Form.Item>
 
-    <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-      <Checkbox>Remember me</Checkbox>
-    </Form.Item>
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
 
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
